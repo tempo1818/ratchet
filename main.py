13dd068c4280d027b1f3df924f1661dadc099321
tempo1818 <cdeshong1616@gmail.com>
2026-03-10 18:41:13 -0700

    initializing

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+import time
+pygame.init()
+
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("My Pygame Window")
+
+time.sleep(5)
